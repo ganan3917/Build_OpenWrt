9b09e17464f1436e58d2caf66f76960f63804915
ganan3917 <134137519+ganan3917@users.noreply.github.com>
2025-10-09 09:48:25 +0800

    Update custom_script.sh

--- scripts/custom_script.sh
+++ scripts/custom_script.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# 修改固件版本号为编译日期
+sed -i "s/^VERSION:=.*/VERSION:=${NEW_VERSION}/" include/version.mk
+sed -i "s/^DISTRIB_DESCRIPTION='.*'/DISTRIB_DESCRIPTION='${NEW_VERSION}'/" package/base-files/files/etc/openwrt_release
+sed -i "s/^DISTRIB_REVISION='.*'/DISTRIB_REVISION='R${CURRENT_DATE}'/" package/base-files/files/etc/openwrt_release
