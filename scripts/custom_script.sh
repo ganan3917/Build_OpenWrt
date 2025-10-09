04cca03aaaea7f417f4f6153e350ab9319376b8c
ganan3917 <134137519+ganan3917@users.noreply.github.com>
2025-10-09 09:52:23 +0800

    Update custom_script.sh

--- scripts/custom_script.sh
+++ scripts/custom_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 获取当前北京时间（格式：YYYYMMDD）
+CURRENT_DATE=$(TZ=Asia/Shanghai date +%Y%m%d)
+# 定义新版本号
+NEW_VERSION="LEDE R${CURRENT_DATE}"
+
 # 修改固件版本号为编译日期
 sed -i "s/^VERSION:=.*/VERSION:=${NEW_VERSION}/" include/version.mk
 sed -i "s/^DISTRIB_DESCRIPTION='.*'/DISTRIB_DESCRIPTION='${NEW_VERSION}'/" package/base-files/files/etc/openwrt_release
